a045716986bcf0a69758347370bd8f648bbb3022
marudor <marudor@marudor.de>
2017-11-25 19:55:00 +0100

    typo

--- packages/eslint-config-joblift/2space.js
+++ packages/eslint-config-joblift/2space.js
@@ -3,6 +3,6 @@ const base = require('./base');
 base.rules['prettier/prettier'] = [
     'error',
     { singleQuote: true, tabWidth: 2, printWidth: 120, semi: true, trailingComma: 'es5' },
-],]
+];
 
 module.export = base;
