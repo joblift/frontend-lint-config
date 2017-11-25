5d9492ab4d680599953566bf84b0f1837e49cda9
marudor <marudor@marudor.de>
2017-11-25 19:46:09 +0100

    allow 2 space prettier

--- .eslintrc.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  extends: './packages/eslint-config-joblift/index.js',
-  env: {
-    node: true,
-  },
-};
--- packages/eslint-config-joblift/2space.js
+++ packages/eslint-config-joblift/2space.js
@@ -2,7 +2,7 @@ const base = require('./base');
 
 base.rules['prettier/prettier'] = [
     'error',
-    { singleQuote: true, tabWidth: 4, printWidth: 120, semi: true, trailingComma: 'es5' },
+    { singleQuote: true, tabWidth: 2, printWidth: 120, semi: true, trailingComma: 'es5' },
 ],]
 
 module.export = base;
