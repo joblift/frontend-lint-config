81ddb2edc388625fde0c564c33f4fd3ee76d6a0f
marudor <marudor@marudor.de>
2018-01-19 12:18:26 +0100

    fix extends

--- packages/eslint-config-joblift/index.js
+++ packages/eslint-config-joblift/index.js
@@ -1,5 +1,5 @@
 module.exports = {
-  extends: ['./base', './flowtype', './jest', './react'],
+  extends: ['./base.js', './flowtype.js', './jest.js', './react.js'],
   rules: {
     'prettier/prettier': ['error',
     { singleQuote: true, tabWidth: 4, printWidth: 120, semi: true, trailingComma: 'es5' }],
