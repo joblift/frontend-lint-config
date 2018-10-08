f7f24cac2d0a612a1ccf2833f47b276540f2eb25
marudor <marudor@marudor.de>
2018-10-08 13:56:06 +0200

    typo

--- packages/eslint-config-joblift/base.js
+++ packages/eslint-config-joblift/base.js
@@ -1,5 +1,5 @@
 module.exports = {
-  extends: ['eslint:recommended', './babel'],
+  extends: ['eslint:recommended', './babel.js'],
   parser: 'babel-eslint',
   parserOptions: {
     sourceType: 'module'
