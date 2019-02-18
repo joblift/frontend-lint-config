5cfd4a48712449592059305027548f4236fdf4bd
marudor <marudor@marudor.de>
2019-02-18 11:38:13 +0100

    Remove unneccesary plugins

--- packages/babel-preset-joblift/index.js
+++ packages/babel-preset-joblift/index.js
@@ -3,13 +3,10 @@ module.exports = () => ({
     '@babel/plugin-proposal-export-default-from',
     ['@babel/plugin-proposal-optional-chaining', { loose: false }],
     ['@babel/plugin-proposal-nullish-coalescing-operator', { loose: false }],
-
     '@babel/plugin-proposal-export-namespace-from',
+    ['@babel/plugin-proposal-class-properties', { loose: false }],
 
     '@babel/plugin-syntax-dynamic-import',
     '@babel/plugin-syntax-import-meta',
-    ['@babel/plugin-proposal-class-properties', { loose: false }],
-    '@babel/plugin-proposal-json-strings',
-    ['@babel/plugin-proposal-object-rest-spread', { loose: false, useBuiltIns: true }],
   ],
 });
