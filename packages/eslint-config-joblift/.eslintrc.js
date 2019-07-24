b682a9be484a845dd93d59a00e0e4efd665aaa61
Reyna DeLogé <reyna.deloge@joblift.de>
2019-07-24 16:37:25 +0200

    remove duplicate config rules

--- packages/eslint-config-joblift/.eslintrc.js
+++ packages/eslint-config-joblift/.eslintrc.js
@@ -5,20 +5,4 @@ module.exports = {
     es6: true,
     node: true
   },
-  globals: {
-    __CLIENT__: false,
-    __SERVER__: false,
-    getStore: false,
-    getState: false,
-    __CAPTCHA_SITEKEY__: false,
-    __GMAPS_API_KEY__: false,
-    __BUILD__: false
-  },
-  rules: {
-    'no-shadow': 0,
-    'import/prefer-default-export': 0,
-    complexity: 0,
-    'comma-dangle': 0,
-    'prefer-arrow-callback': 0
-  }
 };
--- packages/eslint-config-joblift/import.js
+++ packages/eslint-config-joblift/import.js
@@ -3,7 +3,7 @@ module.exports = {
   plugins: ['import'],
   rules: {
     'import/no-mutable-exports': 'error',
-    'import/prefer-default-export': 'error',
+    'import/prefer-default-export': 0,
     'import/first': 'error',
     'import/no-webpack-loader-syntax': 'error'
   }
